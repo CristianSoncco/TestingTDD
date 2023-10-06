cb3b8654ec6095fc5f86043089519e4871e2d1c3
Cristian Soncco <cristian_soncco@msn.com>
2023-10-06 06:16:57 -0500

    imports and expect for firts unit test.

--- test/testSimple.js
+++ test/testSimple.js
@@ -1,6 +1,14 @@
+import chai from 'chai';
+
+import { getYear } from "../controllers/helpers";
+import {expect} from 'chai';
+
  describe('Helpers',()=>{
     //testeamos el método getYear
     describe('Test getYear function',()=>{
-
+        it('Devuelve un número',()=>{
+            const year = getYear();
+            expect(year).to.be.a('number');
+        })
     });
  });
