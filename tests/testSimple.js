128f8e1ae96f352081ef787f273246a1063bd138
Cristian Soncco <cristian_soncco@msn.com>
2023-10-08 06:30:32 -0500

    Comparision of objects.

--- tests/testSimple.js
+++ tests/testSimple.js
@@ -2,6 +2,7 @@ import chai from 'chai';
 
 import { getYear,getHola } from "../controllers/helpers";
 import {expect} from 'chai';
+import IndexController from '../controllers/indexController';
 
  describe('Helpers',()=>{
     //testeamos el método getYear
@@ -40,20 +41,34 @@ import {expect} from 'chai';
         }) 
     });  
     
-    describe.skip('Test tipos de datos',()=>{
+    describe('Test tipos de datos',()=>{
         it('Comparando tipos',()=>{
             const obj = {};
             expect(obj).to.be.a('Object');
-            expect(obj).to.be.a('Array');
+            expect(obj).not.to.be.a('Array');
             let _null;
             expect(_null).to.be.undefined;
             _null=null;
             expect(_null).to.be.null;
         })
 
+        it('Comparando objetos',()=>{
+            const obj1 = {name:'Cristian',last:'Soncco'} ;
+            const obj2 = {name:'Cristian',last:'Soncco'} ;
+            expect(obj1).to.have.property('name');
+            expect(obj1).to.deep.equal(obj2);
+            expect(obj1).to.have.any.keys(['name']);
+            expect(obj1).to.have.all.keys(['name','last']);
+            
+            expect(obj1).to.be.instanceOf(Object);
+
+            const instancia = new IndexController;
+            expect(instancia).to.be.instanceOf(IndexController);
+
+
+        })
+
+
 
     });  
-
-
-
  });
